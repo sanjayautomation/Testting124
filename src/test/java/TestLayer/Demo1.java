8ada2d36fefd5d0de3ad5150ae87b5a578696909
hp <hp@hp-pc>
2023-06-16 20:37:59 +0530

    Adding testing code

---
+++ src/test/java/TestLayer/Demo1.java
@@ -0,0 +1,11 @@
+package TestLayer;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo1 class");
+		System.out.println("Adding code to git hub");
+
+	}
+
+}
