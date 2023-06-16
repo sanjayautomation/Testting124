6fa298885d70e0b111ede86a8b8e6e468fdfad46
hp <hp@hp-pc>
2023-06-16 20:54:57 +0530

    Some changes

--- src/test/java/TestLayer/Demo1.java
+++ src/test/java/TestLayer/Demo1.java
@@ -5,6 +5,8 @@ public class Demo1 {
 	public static void main(String[] args) {
 		System.out.println("Demo1 class");
 		System.out.println("Adding code to git hub");
+		System.out.println("Adding some code demo1 class");// Ashish
+		
 
 	}
 
---
+++ src/test/java/TestLayer/Demo2.java
@@ -0,0 +1,10 @@
+package TestLayer;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo2 class");
+
+	}
+
+}
